4457f1637244016afe833978bbd51208a900d6a8
ErikSkare <skareerik55@gmail.com>
2022-05-08 22:02:18 +0200

    memtrace es gtest

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include "memtrace.h"
-#include "gyujtojegy.hpp"
-#include "vonat.hpp"
 
 using namespace std;
 
 int main()
 {
-    ifstream fs("vonatok.txt");
-    HeteroStore<Vonat> vonatok;
-    fs >> vonatok;
-    fs.close();
-
-    vonatok.bejar(jegyekBeolvasFajlbol("jegyek.txt"));
-
-    Vonat* vonat = vonatok.keres(vonatKeresByAzon(0));
-    try {
-        vonat->jegyHozzaad(new GyujtoJegy(vonat, 5, 5, 2));
-    } catch(FoglaltHiba&) {
-        std::cout << "hello";
-    }
-    vonat->jegyekKiir(cout);
+    int *a = new int;
+    delete a;
     return 0;
 }
