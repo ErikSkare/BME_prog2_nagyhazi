bccd660e3e0880ed3785282733a50d3cf43ba2c1
ErikSkare <skareerik55@gmail.com>
2022-05-09 17:15:44 +0200

    menu.hpp kommentek

--- menu.hpp
+++ menu.hpp
@@ -9,6 +9,7 @@
 
 #include "vonat.hpp"
 
+/// A menü állapota
 enum MenuAllapot {
     FOMENU = 0,
     VONAT_HOZZAADAS = 1,
@@ -18,18 +19,22 @@ enum MenuAllapot {
 };
 
 class Menu {
-    MenuAllapot allapot;
-    HeteroStore<Vonat>& vonatok;
+    MenuAllapot allapot; ///> A menü aktuális állapota
+    HeteroStore<Vonat>& vonatok; ///> A vonatok heterogén kollekció, amin a mûveleteket végezzük
 
 public:
+    /// Konstruktor
     Menu(HeteroStore<Vonat>& vonatok)
         : allapot(FOMENU), vonatok(vonatok) { }
 
+    /// Megjeleníti a menüt, akkor lép ki, ha KILEPES allapotba került.
     void megjelenit();
 
+    /// virtuális destruktor
     virtual ~Menu() { }
 
 private:
+    /// Vezérlõ függvények, visszaadják a következõ állapotot
     MenuAllapot fomenu_vezerlo();
     MenuAllapot vonat_hozzaadas_vezerlo();
     MenuAllapot jegy_kiadas_vezerlo();
